76c842d34b4336b5ff0722234e9c663dcd32b5c8
Woojin Kim <woojinwork16@gmail.com>
2025-04-02 15:23:42 +0900

    Update Dockerfile&docker-compose, Add test code

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pymysql
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,27 @@ app = Flask(__name__)
 def test():
     return "Docker Container TEST"
 
+def get_db_connection():
+    return pymysql.connect(
+        host="db",
+        user="woojin",
+        password="12345678*",
+        database="test_db",
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.route("/users")
+def get_users():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users;")
+        users = cursor.fetchall()
+        conn.close()
+        return jsonify(users)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, email)
+VALUES  
+    ('ssumisfree', 'ssumisfree@example.com'),
+    ('woojin', 'woojinwork16@gmail.com'),
+    ('rest-point', 'rest-point@example.com'),
+    ('kim_tae_young', 'kim_tae_young@example.com');
