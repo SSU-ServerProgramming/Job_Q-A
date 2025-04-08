1c551a187b6d8e58b0515cfd1346256525117cd5
Woojin Kim <woojinwork16@gmail.com>
2025-04-08 21:15:41 +0900

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,34 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     company VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS questions (
-    question_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    title VARCHAR(50),
-    category VARCHAR(50),
-    content VARCHAR(300),
-    likes INT DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS questions (
+    question_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    content VARCHAR(300) NOT NULL,
+    likes INT DEFAULT 0 CHECK (likes >= 0),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
 
 CREATE TABLE IF NOT EXISTS comments (
     comment_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    question_id INT,
-    content VARCHAR(300),
-    likes INT DEFAULT 0,
+    user_id INT NOT NULL,
+    question_id INT NOT NULL,
+    content VARCHAR(300) NOT NULL,
+    likes INT DEFAULT 0 CHECK (likes >= 0),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (question_id) REFERENCES questions(question_id)
