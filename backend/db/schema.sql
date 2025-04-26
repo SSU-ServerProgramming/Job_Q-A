9ddae1e7889adb6634c9e440d5874568b21c6203
Taeyoung Kim <legends0427@gmail.com>
2025-04-26 19:23:28 +0900

    Chore : backend/db 파일 수정

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,38 +1,54 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id INT AUTO_INCREMENT,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    company VARCHAR(50),
-    PRIMARY KEY (user_id)
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `companies` (
+  `company_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`company_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS categories (
-    category_id INT AUTO_INCREMENT,
-    category_name VARCHAR(30) UNIQUE NOT NULL,
-    PRIMARY KEY (category_id)
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `nickname` varchar(45) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `company_id` int NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  CONSTRAINT `fk_users_company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS questions (
-    question_id INT AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    category_id INT NOT NULL,
-    title VARCHAR(50) NOT NULL,
-    content VARCHAR(300) NOT NULL,
-    likes INT DEFAULT 0 CHECK (likes >= 0),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (question_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS comments (
-    comment_id INT AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    question_id INT NOT NULL,
-    content VARCHAR(300) NOT NULL,
-    likes INT DEFAULT 0 CHECK (likes >= 0),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (comment_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (question_id) REFERENCES questions(question_id)
-) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `boards` (
+  `board_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `category_id` int NOT NULL,
+  `content` varchar(2000) DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `like` int DEFAULT '0',
+  `comment_count` int DEFAULT '0',
+  PRIMARY KEY (`board_id`),
+  KEY `user_id_idx` (`user_id`),
+  KEY `category_id_idx` (`category_id`),
+  CONSTRAINT `fk_boards_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_boards_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `board_id` int NOT NULL,
+  `content` varchar(200) NOT NULL,
+  `date` datetime NOT NULL,
+  `like` int DEFAULT '0',
+  `parent_comment_id` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`comment_id`),
+  KEY `board_id_idx` (`board_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `fk_comments_board_id` FOREIGN KEY (`board_id`) REFERENCES `boards` (`board_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_comments_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,6 +1,13 @@
-INSERT INTO users (username, email, company)
+INSERT INTO companies(name)
+VALUES
+    ('취준'),
+    ('Naver'),
+    ('Kakao'),
+    ('Line');
+
+INSERT INTO users (nickname, email, password, company_id)
 VALUES  
-    ('ssumisfree', 'ssumisfree@example.com', 'Naver'),
-    ('woojin', 'woojinwork16@gmail.com', 'Kakao'),
-    ('rest-point', 'rest-point@example.com', 'Line'),
-    ('kim_tae_young', 'kim_tae_young@example.com', 'Coupang');
+    ('ssumisfree', 'ssumisfree@example.com', 'password1', 1),
+    ('woojin', 'woojinwork16@gmail.com', 'password2', 2),
+    ('rest-point', 'rest-point@example.com', 'password3', 3),
+    ('kim_tae_young', 'kim_tae_young@example.com', 'password4', 4);
