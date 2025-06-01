f385f5b318eb94aaffba550231aeb3a332fe5925
Woojin Kim <woojinwork16@gmail.com>
2025-06-01 17:13:15 +0900

    fix: comment_likes commit->flush 수정

--- backend/app/persistence/repositories/comment_like.py
+++ backend/app/persistence/repositories/comment_like.py
@@ -17,11 +17,11 @@ class CommentLikesRepository(BaseRepository):
     def insert_like(self, user_id: int, comment_id: int) -> CommentLike:
         like = CommentLike(user_id=user_id, comment_id=comment_id)
         self.session.add(like)
-        self.session.commit()
+        self.session.flush()
         return like
 
     def delete_like(self, user_id: int, comment_id: int) -> None:
         like = self.get_by_user_comment_id(user_id, comment_id)
         if like is not None:
             self.session.delete(like)
-        self.session.commit()
+        self.session.flush()
