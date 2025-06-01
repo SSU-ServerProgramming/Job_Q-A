7d4f693657f26a6252207f6707f25e68ae0d5ebd
Woojin Kim <woojinwork16@gmail.com>
2025-06-02 00:39:56 +0900

    feat: persistence 예외처리

--- backend/app/persistence/repositories/board.py
+++ backend/app/persistence/repositories/board.py
@@ -1,15 +1,18 @@
 from sqlalchemy import or_
 
+from app.persistence.exception import PersistenceError
 from .base import BaseRepository
 from app.database.models.board import Board
 from app.database.models.board_likes import BoardLikes
 
 
 class BoardRepository(BaseRepository):
+    @PersistenceError.error
     def get_by_id(self, board_id: int) -> Board | None:
         """ID로 게시글을 조회합니다."""
         return self.session.query(Board).filter(Board.id == board_id).first()
     
+    @PersistenceError.error
     def get_by_user_id(self, user_id: int) -> list[Board]:
         """User ID로 게시글을 최신 생성순으로 조회합니다."""
         result = (
@@ -20,6 +23,7 @@ class BoardRepository(BaseRepository):
         )
         return result
     
+    @PersistenceError.error
     def get_by_category_id(self, category_id: int, skip: int = 0, limit: int = 100) -> list[Board]:
         """Category ID로 게시글을 최신 생성순으로 조회합니다.(페이징)"""
         result = (
@@ -30,6 +34,7 @@ class BoardRepository(BaseRepository):
         )
         return result
     
+    @PersistenceError.error
     def get_by_keyword(self, keyword: str, skip: int = 0, limit: int = 100) -> list[Board]:
         """제목이나 본문에 키워드가 포함된 게시글을 최신 생성순으로 조회합니다.(페이징)"""
         result = (
@@ -45,6 +50,7 @@ class BoardRepository(BaseRepository):
         )
         return result
 
+    @PersistenceError.error
     def get_by_num_like(self, skip: int = 0, limit: int = 100) -> list[Board]:
         """좋아요 개수가 많은 순으로 정렬되어 게시글을 조회합니다(페이징)"""
         result = (
@@ -56,6 +62,7 @@ class BoardRepository(BaseRepository):
         )
         return result
     
+    @PersistenceError.error
     def get_by_num_comment(self, skip: int = 0, limit: int = 100) -> list[Board]:
         """좋아요 개수가 많은 순으로 정렬되어 게시글을 조회합니다(페이징)"""
         result = (
@@ -67,6 +74,7 @@ class BoardRepository(BaseRepository):
         )
         return result
     
+    @PersistenceError.error
     def get_all_boards(self, skip: int = 0, limit: int = 100) -> list[Board]:
         """최근 생성된 순으로 게시글을 조회합니다.(페이징)"""
         result = (
@@ -78,18 +86,21 @@ class BoardRepository(BaseRepository):
         )
         return result
     
+    @PersistenceError.error
     def create(self, board: Board) -> Board:
         """새로운 게시글(Board)을 생성합니다."""
         self.session.add(board)
         self.session.flush()
         return board
     
+    @PersistenceError.error
     def update(self, board: Board) -> Board:
         """기존 게시글을 업데이트 합니다."""
         self.session.merge(board)
         self.session.flush()
         return board
     
+    @PersistenceError.error
     def delete(self, board_id: int) -> None:
         """게시글을 삭제합니다."""
         board = self.get_by_id(board_id)
