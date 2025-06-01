a3a67362c736fd96ff2a836c9bd6aa9d4135eb02
Woojin Kim <woojinwork16@gmail.com>
2025-06-01 22:10:05 +0900

    feat: Company Repo 구현

--- backend/app/persistence/repositories/company.py
+++ backend/app/persistence/repositories/company.py
@@ -1,6 +1,9 @@
 from .base import BaseRepository
 from app.database.models.company import Company
 
-class CompanyRepository(BaseRepository):
-    def get_by_domain(self, domain: str) -> Company | None:
+class CompanyRepository(BaseRepository):    
+    def get_by_domain(self, domain:str) -> Company | None:
         return self.session.query(Company).filter(Company.domain == domain).first()
+    
+    def get_by_id(self, id:int) -> Company | None:
+        return self.session.query(Company).filter(Company.id == id).first()
